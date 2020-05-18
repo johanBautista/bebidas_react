194dca2e551e0998586558c4c54c4d4df58c73c3
johan Bautista <jsbparra@gmail.com>
2020-05-18 10:35:12 +0200

    cmp formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,9 +2,36 @@ import React from 'react';
 
 const Formulario = () => {
   return (
-    <div>
-      <h1>eii</h1>
-    </div>
+    <form className="col-12">
+      <fieldset className="text-center">
+        <legend>BUSCA CATEGORIA O INGREDIENTE</legend>
+      </fieldset>
+
+      <div className="row mt-4">
+        <div className="col-md-4">
+          <input
+            name="nombre"
+            className="form-control"
+            placeholder="Buscar por Ingrediente"
+            type="text"
+          />
+        </div>
+
+        <div className="col-md-4">
+          <select name="categoria" className="form-control">
+            <option value="">-- Seleccione Categoria</option>
+          </select>
+        </div>
+
+        <div className="col-md-4">
+          <input
+            type="submit"
+            className="btn btn-block btn-primary"
+            value="BUSCAR"
+          />
+        </div>
+      </div>
+    </form>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,14 @@ const Header = ({ titulo }) => {
   return (
     <header className="bg-alert">
       <h1>{titulo}</h1>
+      <a
+        className="io"
+        href="https://www.thecocktaildb.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+         The coctaill DB
+      </a>
     </header>
   );
 };
