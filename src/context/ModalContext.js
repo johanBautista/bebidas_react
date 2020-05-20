5cb74e0995182a8890bb2713298ac2605a1b4c28
johan Bautista <jsbparra@gmail.com>
2020-05-20 17:26:10 +0200

    llamar api para obtener detalle de receta

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -4,10 +4,25 @@ import axios from 'axios';
 export const ModalContext = createContext();
 
 const ModalProvider = (props) => {
-  // state 
-  const [idreceta, guardarIdReceta] = useState(null)
+  // state
+  const [idreceta, guardarIdReceta] = useState(null);
+  const [receta, guardarReceta] = useState({});
+
+  //
+  useEffect(() => {
+    const obtenerReceta = async () => {
+      if (!idreceta) return;
+      const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idreceta}`;
+      const resultado = await axios.get(url);
+
+      // console.log(resultado.data.drinks[0]);
+      guardarReceta(resultado.data.drinks[0]);
+    };
+    obtenerReceta();
+  }, [idreceta]);
+
   return (
-    <ModalContext.Provider value={{guardarIdReceta}}>
+    <ModalContext.Provider value={{ guardarIdReceta }}>
       {props.children}
     </ModalContext.Provider>
   );
