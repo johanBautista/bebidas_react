213d3396684cf0ca0734a127d583f508344809c1
johan Bautista <jsbparra@gmail.com>
2020-05-20 10:54:54 +0200

    imprimir recetas

--- src/components/Receta.js
+++ src/components/Receta.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const Receta = ({recetas}) => {
+const Receta = ({ receta }) => {
   return (
-    <div>
-      <h1>eii</h1>
+    <div className="col-md-4 mb-3">
+      <div className="card">
+        <h2 className="card-header">{receta.strDrink}</h2>
+
+        <img
+          className="card-img-top"
+          src={receta.strDrinkThumb}
+          alt={`Imagen de ${receta.strDrink}`}
+        />
+
+        <div className="card-body">
+          <button type="button" className="btn btn-block btn-primary">Ver Receta </button>
+        </div>
+      </div>
     </div>
   );
 };
